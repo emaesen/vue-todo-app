762e10bf989ce0b12456a46e4039b5e1b4625003
Edward <edward.maesen@gmail.com>
2018-11-02 10:16:20 -0700

    Minor text change

--- src/components/CreateDummyTodos.vue
+++ src/components/CreateDummyTodos.vue
@@ -3,7 +3,7 @@
     <hr class="ui">
     <div class="ui">
       Create a few dummy Todos:<br>
-      (for development purposes)
+      (for development/test purposes)
     </div>
     <div class="ui action input">
       <input
